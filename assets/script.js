f8e122a95b2310466907d5aca84ba2d665d1ffe1
GHBash <hiebert.garrett67@gmail.com>
2020-05-09 10:57:49 -0700

    Added rendering functionality for saved tracklist

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
-// 
+function renderTrackList(trackList) {
+    var tracksEl = $("#tracks");
+    tracksEl.empty();
+    var ulTracksEl = $("<ul>");
+    for (var i = 0; i < trackList.length; i++) {
+        var liTracksEl = $("<li>");
+        liTracksEl.text(trackList[i]);
+        ulTracksEl.append(liTracksEl);
+    }
+    tracksEl.append(ulTracksEl);
+}
+
+var testTrackList = ["Darcy's Donkey", "Cornfield Chase", "There and Back Again", "Sweden", "Island Life"];
+renderTrackList(testTrackList);
