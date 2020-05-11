14405bad1a32cc12a73b8e64458b54339d325eca
Luis Cervantes <luiscerv1229@gmail.com>
2020-05-11 13:17:24 -0700

    modified html and css with more flex utilities and background

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,33 @@
-// 
+//Listeners
+$("body").on("click", ".track", function() {
+    var songName = $(this).text();
+    console.log("Song clicked is: " + songName);
+})
+
+$("#save").on("click", function() {
+
+    var artist = $(this).prev().val();
+    console.log("Searching for: " + artist);
+
+})
+
+
+
+function renderTrackList(trackList) {
+    var tracksEl = $("#tracks");
+    tracksEl.empty();
+    var ulTracksEl = $("<ul>");
+    for (var i = 0; i < trackList.length; i++) {
+        var liTracksEl = $("<li>").addClass("track");
+        liTracksEl.text(trackList[i]);
+        ulTracksEl.append(liTracksEl);
+    }
+    tracksEl.append(ulTracksEl);
+}
+
+
+
+
+var testTrackList = ["Darcy's Donkey", "Cornfield Chase", "There and Back Again", "Sweden", "Island Life"];
+renderTrackList(testTrackList);
+
