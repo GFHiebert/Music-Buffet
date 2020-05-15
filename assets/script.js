697ae76591f47a30fadca6d4d3b0a46cc070b295
Luis Cervantes <luiscerv1229@gmail.com>
2020-05-14 19:35:58 -0700

    added search clear onclick and simartist clear on click

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 let discoverBtn = document.getElementById("discover");
 let apikey = "368598-musicbuf-RZ4G3NI6";
-let addBtn = document.getElementById("add")
+let addBtn = document.getElementById("add");
 
 jQuery.ajaxPrefilter(function (options) {
   if (options.crossDomain && jQuery.support.cors) {
@@ -20,47 +20,116 @@ function getArtist() {
     $.ajax({
       url: queryURL,
       method: "GET",
-    }).done(function (response) {
+    }).then(function (response) {
       console.log(response);
       for (let i = 0; i < response.Similar.Results.length; i++)
         $("#artists").append(
           "<li>" + response.Similar.Results[i].Name + "</li>"
         );
+        // var clickLi = $('<li>').addClass('like');
+        // clickLi.append('#trackList');
+
+      $("#newItem").val("");
     });
+    $('#artists').empty();
   });
 }
-$("body").on("click", ".track", function () {
-    var songName = $(this).text();
-    console.log("Song clicked is: " + songName);
-  });
-  
-  $("#save").on("click", function () {
-    var artist = $(this).prev().val();
-    console.log("Searching for: " + artist);
-  });
-  
-  function renderTrackList(trackList) {
-    var tracksEl = $("#tracks");
-    tracksEl.empty();
-    var ulTracksEl = $("<ul>");
-    for (var i = 0; i < trackList.length; i++) {
-      var liTracksEl = $("<li>").addClass("track");
-      liTracksEl.text(trackList[i]);
-      ulTracksEl.append(liTracksEl);
-    }
-    tracksEl.append(ulTracksEl);
+function renderFavArtistList(favArtistList) {
+  var artistsEl = $("#tracks");
+  artistsEl.empty();
+  var ulArtistsEl = $("<ul>");
+  for (var i = 0; i < favArtistList.length; i++) {
+    var liArtistsEl = $("<li>").addClass("artist");
+    liArtistsEl.text(favArtistList[i]);
+    ulArtistsEl.append(liArtistsEl);
   }
-  
-  var testTrackList = [
-    "Darcy's Donkey",
-    // "Cornfield Chase",
-    // "There and Back Again",
-    // "Sweden",
-    // "Island Life",
-  ];
-  renderTrackList(testTrackList);
-  
+  artistsEl.append(ulArtistsEl);
+}
+
+var testFavArtistList = [
+  "Gaelic Storm",
+  "The Dubliners",
+  "Hans Zimmer",
+  "Howard Shore",
+  "C418",
+];
+renderFavArtistList(testFavArtistList);
+
+
+
+let accessToken;
+
+function buildAuthLink() {
+  var artist = $("#newItem").val();
+  const queryURL =
+    "https://api.spotify.com/v1/search?query=" +
+    artist +
+    "&offset=0&limit=20&type=artist";
+  const hash = window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce(function (initial, item) {
+      if (item) {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+    }, {});
+  window.location.hash = "";
+
+  let _token = hash.access_token;
+
+  const authEndpoint = "https://accounts.spotify.com/authorize";
+  const clientID = "87da17f3514b4a86854820f3d7804bb0";
+  const redirectURI = "https://gfhiebert.github.io/Music-Buffet/";
+  const scopes = ["user-read-private", "user-read-email"];
+
+  if (!_token) {
+    window.location = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scopes.join(
+      "%20"
+    )}&response_type=token&show_dialog=true`;
+  }
+
+  // Spotify API
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer " + _token);
+    },
+    success: function (response) {
+      console.log(response);
+    },
+  });
+}
+
+// Event Trigger for Spotify Auth
+var authButton = $("#widget").append($("<button>").html("Allow Access"));
+authButton.click(function () {
+  buildAuthLink();
+});
+
+
+
+
+var URI = "4ZA0jcRUrVPupPnyV66aoI"
+
+
+
+function iFrameW() {
+  $("#widget").empty();
+  var iFrameW = $("<iframe>").attr({
+      src: "https://open.spotify.com/embed/track/" + URI,
+      width: "300",
+      height: "80",
+      frameborder: "0",
+      allowtransparency: "true",
+      allow: "encrypted-media"
+  })
+  $("#widget").append(iFrameW)
+}
+
+iFrameW();
 
 
 getArtist();
-
